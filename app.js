49bd1afe2ab3c26cae872822bde6ea15e873efe7
FarheenMalak <115576023+FarheenMalak@users.noreply.github.com>
2024-08-12 13:08:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
         document.write( "<h1>Rules for naming JS variables </h1>");
         
         // Displaying the rules
-        document.write = ("Variable names can only contain letters, numbers, $ and _. <br>For example: $my_1stVariable <br/>");
-        document.write = ("Variables must begin with a letter, $ or _. <br>For example: $name, _name, or name");
-        document.write = ("Variable names are case sensitive.");
-        document.write= ("Variable names should not be JS keywords.");
+        document.write("Variable names can only contain letters, numbers, $ and _. <br>For example: $my_1stVariable <br/>");
+        document.write("Variables must begin with a letter, $ or _. <br>For example: $name, _name, or name");
+        document.write("Variable names are case sensitive.");
+        document.write("Variable names should not be JS keywords.");
